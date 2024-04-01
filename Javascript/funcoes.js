740f037ba685f392b3fb19e986f5cb951961fbfb
ALLAN SOBRAL DA SILVA <allan.ssilva4@senacsp.edu.br>
2024-04-01 20:12:54 -0300

    Primeiro Commit

---
+++ Javascript/funcoes.js
@@ -0,0 +1,76 @@
+function somar(){
+    var num1;//Declarando uma variável 
+    var num2;
+    var res;
+    //Coletando os dados dos campos
+    num1 = parseInt(document.getElementById("tpNum").value);
+    num2 = parseInt(document.getElementById("tsNum").value);
+    //Realizando o cálculo
+    res = num1 + num2;
+    document.getElementById("resultado").innerHTML = "A soma dos números é: " + res;
+}//fim do método
+
+function subtrair(){
+    var num1;//Declarando uma variável 
+    var num2;
+    var res;
+    //Coletando os dados dos campos
+    num1 = parseInt(document.getElementById("tpNum").value);
+    num2 = parseInt(document.getElementById("tsNum").value);
+    //Realizando o cálculo
+    res = num1 - num2;
+    document.getElementById("resultado").innerHTML = "A subtração dos números é: " + res;
+}//fim do método
+
+function multiplicar(){
+    var num1;//Declarando uma variável 
+    var num2;
+    var res;
+    //Coletando os dados dos campos
+    num1 = parseInt(document.getElementById("tpNum").value);
+    num2 = parseInt(document.getElementById("tsNum").value);
+    //Realizando o cálculo
+    res = num1 * num2;
+    document.getElementById("resultado").innerHTML = "A multiplicação dos números é: " + res;
+}//fim do método
+
+function dividir(){
+    var num1;//Declarando uma variável 
+    var num2;
+    var res;
+    //Coletando os dados dos campos
+    num1 = parseInt(document.getElementById("tpNum").value);
+    num2 = parseInt(document.getElementById("tsNum").value);
+    //Realizando o cálculo
+    if(num2 <= 0){
+        res = "Impossível dividir por número menores ou iguais a zero!";
+    }else{
+        res = num1 / num2;
+    }
+    //Faço a resposta
+    document.getElementById("resultado").innerHTML = "A divisão dos números é: " + res;
+}//fim do método
+
+function tabuada(){
+    var num1;
+    var num2;
+
+    num1 = parseInt(document.getElementById("tpNum").value);
+    num2 = parseInt(document.getElementById("tsNum").value);
+
+    var resNum1 = "Tabuada do " + num1;
+    var resNum2 = "Tabuada do " + num2;
+
+    for(i = 0; i <= 10; i++){
+        resNum1 += "\n" + num1 + " * " + i + " = " + (num1 * i);
+        resNum2 += "\n" + num2 + " * " + i + " = " + (num2 * i);
+    }
+
+    document.getElementById("resultado").innerHTML = "\n" + resNum1 + "\n\n" + resNum2;
+}//fim do método
+
+//Faça uma função para calcular a potência
+
+//Faça uma função para calcular a raiz dos dois números
+
+//Faça uma função para calcular a fórmula de bhaskara
